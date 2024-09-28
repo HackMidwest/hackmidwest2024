07a00f4d2991e32156522f5f5b78833d08df367a
Landry Geiger <landry.geiger@gmail.com>
2024-09-28 13:46:54 -0500

    Init websocket server

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,14 +1,30 @@
 import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
 const port = 4000;
 
-// Define a simple Hello World endpoint
+const server = http.createServer(app);
+const io = new Server(server);
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`App is listening at http://localhost:${port}`);
+app.post('/api/join', (req, res) => {
+  console.log(req);
+  res.sendStatus(200);
+});
+
+io.on('connection', socket => {
+  console.log(`Socket with id ${socket.id} has connected.`);
+
+  socket.on('disconnect', () => {
+    console.log(`Socket with id ${socket.id} has disconnected.`);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`App is listening on port ${port}...`);
 });
