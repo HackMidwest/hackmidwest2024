8a56065c71889aa31595c1563c18dc30f312f65c
john <johnmichaelpost@gmail.com>
2024-09-28 14:14:57 -0500

    add some basic reducer actions and supporting code

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,7 +1,18 @@
 import express from 'express';
+import { App } from '../common/types';
+import { Mutex } from 'async-mutex';
+import { SetAppState } from '../common/transitions';
 
 const app = express();
 const port = 4000;
+
+let state: App = { kind: 'waitingLobby', players: [] };
+const stateMutex = new Mutex();
+const setState: SetAppState = async (getNewState: (prev: App) => App) => {
+  const release = await stateMutex.acquire();
+  state = getNewState(state);
+  release();
+};
 
 // Define a simple Hello World endpoint
 app.get('/', (req, res) => {
