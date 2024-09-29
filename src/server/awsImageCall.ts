bb2d84cb3e02c4d1a8c283b7d65f0d6603aa262e
Roy Dumblauskas <roydumblauskas@gmail.com>
2024-09-28 21:19:09 -0500

    AI and Pinata endpoints completed

--- src/server/awsImageCall.ts
+++ src/server/awsImageCall.ts
@@ -3,26 +3,47 @@ import {
   InvokeModelCommand,
   InvokeModelCommandInput,
 } from '@aws-sdk/client-bedrock-runtime';
+import { PinataSDK } from 'pinata';
+import { v4 } from 'uuid';
 
-const client = new BedrockRuntimeClient({
-  region: process.env.AWS_DEFAULT_REGION as string,
-  credentials: {
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-    sessionToken: process.env.AWS_SESSION_TOKEN as string,
-  },
+const pinata = new PinataSDK({
+  pinataJwt: `${import.meta.env.VITE_PINATA_JWT}`,
+  pinataGateway: `${import.meta.env.VITE_GATEWAY_URL}`,
 });
 
-const modelId = process.env.IMAGE_MODEL_ID as string;
-const prompt = '"Rubber Duck"';
-const enclosed_prompt = 'Human: ' + prompt + '\n\nAssistant:';
-const modelInput: InvokeModelCommandInput = {
-  modelId: modelId,
-  contentType: 'application/json',
-  accept: 'application/json',
-  body: JSON.stringify({
-    prompt: enclosed_prompt,
-  }),
-};
+export const callStableImage = async (prompt: string) => {
+  const client = new BedrockRuntimeClient({
+    region: process.env.AWS_DEFAULT_REGION as string,
+    credentials: {
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+      sessionToken: process.env.AWS_SESSION_TOKEN as string,
+    },
+  });
 
-client.send(new InvokeModelCommand(modelInput)).then(console.log);
+  const modelId = process.env.IMAGE_MODEL_ID as string;
+  const enclosed_prompt = 'Human: ' + prompt + '\n\nAssistant:';
+
+  const modelInput: InvokeModelCommandInput = {
+    modelId: modelId,
+    contentType: 'application/json',
+    accept: 'application/json',
+    body: JSON.stringify({
+      prompt: enclosed_prompt,
+    }),
+  };
+
+  const response = (await client.send(new InvokeModelCommand(modelInput))).body;
+
+  const blob = new Blob([response]);
+  const file = new File([blob], v4());
+
+  const upload = await pinata.upload.file(file);
+
+  const signedUrl = await pinata.gateways.createSignedURL({
+    cid: upload.cid,
+    expires: 30 * 60, // seconds the link is valid for
+  });
+
+  return signedUrl;
+};
