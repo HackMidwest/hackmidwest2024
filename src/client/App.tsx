d3a1f7a260810202f4fa97583a2c49b17f089f18
ZWally <zach.wallenburg@icloud.com>
2024-09-28 21:22:55 -0500

    add dynamic background

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,9 +1,11 @@
+import Background from './components/Background';
 import Router from './components/Router';
 import { StateContextProvider } from './State';
 
 export const App = () => {
   return (
     <StateContextProvider>
+      <Background></Background>
       <Router />
     </StateContextProvider>
   );
---
+++ src/client/components/Background.tsx
@@ -0,0 +1,27 @@
+import './Background.css'
+const Background = () => {
+    return(
+        <ul className="background">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        </ul>)
+};
+
+export default Background;
